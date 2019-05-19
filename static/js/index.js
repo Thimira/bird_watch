19f06c1b0c3f12b469badef895d185254fd47337
Thimira Amaratunga <thimiraamara@gmail.com>
2019-05-19 12:15:18 +0530

    remove tast message and added an inline message

--- static/js/index.js
+++ static/js/index.js
@@ -11,7 +11,7 @@ $(function() {
             contentType: "application/json; charset=utf-8",
             dataType: "json",
             success: function(data) {
-                $('#correct-feedback').toast('show')
+                $('#feedback-ack').show();
             },
             failure: function(errMsg) {
                 // alert(errMsg);
@@ -21,16 +21,35 @@ $(function() {
 
     $("#pred-correct").click( function(event) {
         event.preventDefault();
-        setCorrectness(1)
+        $("#pred-correct").removeClass("badge-secondary");
+        $("#pred-correct").addClass("badge-success");
+        $("#pred-incorrect").removeClass("badge-danger");
+        $("#pred-incorrect").addClass("badge-secondary");
+        $("#pred-notsure").removeClass("badge-info");
+        $("#pred-notsure").addClass("badge-secondary");
+        setCorrectness(1);
+
     });
 
     $("#pred-incorrect").click( function(event) {
         event.preventDefault();
-        setCorrectness(0)
+        $("#pred-correct").removeClass("badge-success");
+        $("#pred-correct").addClass("badge-secondary");
+        $("#pred-incorrect").removeClass("badge-secondary");
+        $("#pred-incorrect").addClass("badge-danger");
+        $("#pred-notsure").removeClass("badge-info");
+        $("#pred-notsure").addClass("badge-secondary");
+        setCorrectness(0);
     });
 
     $("#pred-notsure").click( function(event) {
         event.preventDefault();
-        setCorrectness(-1)
+        $("#pred-correct").removeClass("badge-success");
+        $("#pred-correct").addClass("badge-secondary");
+        $("#pred-incorrect").removeClass("badge-danger");
+        $("#pred-incorrect").addClass("badge-secondary");
+        $("#pred-notsure").removeClass("badge-secondary");
+        $("#pred-notsure").addClass("badge-info");
+        setCorrectness(-1);
     });
 });
